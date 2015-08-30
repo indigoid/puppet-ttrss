030bb75ade3a854fee483574b24271ee0074a5c2
John Slee <john.slee@fairfaxmedia.com.au>
2015-08-31 01:10:46 +1000

    WARS HAVE BEGUN THAT WAY, MISTER AMBASSADOR

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -1,27 +1,42 @@
+define ttrss::webserver::php (
+  $vhost,
+  $www_root    = '/var/www',
+  $backend     = "127.0.0.1:9000",
+  $listen_port = 80,
+  $index_files = ['index.php', 'index.html', 'index.htm'],
+) {
+  nginx::resource::vhost { $vhost:
+    ensure      => present,
+    listen_port => $listen_port,
+    www_root    => $www_root,
+  }
+  nginx::resource::location { "${vhost}_root":
+    ensure          => present,
+    vhost           => $vhost,
+    www_root        => $www_root,
+    location        => '~ \.php$',
+    index_files     => $index_files
+    proxy           => undef,
+    fastcgi         => $backend,
+    fastcgi_script  => undef,
+      location_cfg_append => {
+       fastcgi_connect_timeout => '3m',
+       fastcgi_read_timeout    => '3m',
+       fastcgi_send_timeout    => '3m'
+    }
+  }
+}
+
 class ttrss::webserver (
   $vhost,
   $docroot = '/opt/tt',
   $port    = 80,
 ) {
   include ::php
-  class { '::apache':
-    default_vhost => false
-  }
-
-  ::apache::fastcgi::server { 'php':
-    host       => '127.0.0.1:9000',
-    timeout    => 15,
-    flush      => false,
-    faux_path  => '/var/www/php.fcgi',
-    fcgi_alias => '/php.fcgi',
-    file_type  => 'application/x-httpd-php',
-  }
-
-  ::apache::vhost { $vhost:
-    docroot         => $docroot,
-    default_vhost   => true,
-    port            => $port,
-    override        => 'all',
-    custom_fragment => 'AddType application/x-httpd-php .php',
+  include ::nginx
+  ::ttrss::webserver::php { $vhost:
+    vhost       => $vhost,
+    www_root    => $docroot,
+    listen_port => $port,
   }
 }
